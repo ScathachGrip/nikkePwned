2b57789f47c37f365c278dadfd1c7a0f850b3f22
sinkaroid <bibsindra@gmail.com>
2025-03-08 20:20:35 +0700

    style: updating linting and formatting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,23 +3,19 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: [
-      "resources/**",
-      "**/*.mjs",
-      "**/*.cjs"
-    ],
+    ignores: ["resources/**", "**/*.mjs", "**/*.cjs"],
   },
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
     rules: {
-      "quotes": ["error", "double"],
-      "semi": ["error", "always"],
-      "indent": ["error", 2],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      indent: ["error", 2],
       "no-empty": "error",
       "no-func-assign": "error",
       "no-unreachable": "error",
-      "template-curly-spacing": ["error", "never"]
-    }
-  }
+      "template-curly-spacing": ["error", "never"],
+    },
+  },
 );
